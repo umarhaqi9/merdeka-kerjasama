9ea8fe913e233309b9569fac24953f404200b8bd
umarhaqi9 <74255063+umarhaqi9@users.noreply.github.com>
2022-12-08 11:37:15 +0700

    Initial commit again

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import sharedStyles from '~/styles/tailwind.css';
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
@@ -29,4 +31,8 @@ export default function App() {
       </body>
     </html>
   );
+}
+
+export function links(){
+  return[{rel: 'stylesheet', href: sharedStyles},]
 }
